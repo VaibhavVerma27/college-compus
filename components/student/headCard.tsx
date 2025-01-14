fe36a1518afe94cc92e6485437e6d94b666f0adb
Antriksh Gupta <antriksg007@gmail.com>
2025-01-14 23:50:41 +0530

    .

--- components/student/headCard.tsx
+++ components/student/headCard.tsx
@@ -6,13 +6,13 @@ interface Props {
 
 export default function HeadCard({ name, student_id, profile }: Props) {
   return (
-    <div className="flex flex-row items-center justify-around m-5 p-6 w-full max-w-xl bg-gradient-to-b from-[#0B0C10] to-[#1F2833] border-2 rounded-xl border-cyan-300 shadow-lg shadow-cyan-300/50 flex-shrink-0">
+    <div className="flex flex-row items-center justify-around m-5 p-6 w-full max-w-xl bg-gradient-to-b from-[#0B0C10] to-[#1F2833] border-2 rounded-xl border-blue-500 shadow-lg shadow-blue-900 flex-shrink-0">
       <img
         src={profile || "https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg"}
-        className="w-16 h-16 border-4 rounded-full border-cyan-300 shadow-md shadow-cyan-300/50"
+        className="w-16 h-16 border-4 rounded-full  border-blue-500 shadow-md shadow-blue-900"
         alt="Profile"
       />
-      <div className="text-lg md:text-xl font-bold text-[#66FCF1] text-center">
+      <div className="text-lg md:text-xl font-bold border-blue-500 text-blue-500 font-archivo text-center">
         {name}
       </div>
       <div className="text-lg md:text-xl font-bold text-white text-center">
