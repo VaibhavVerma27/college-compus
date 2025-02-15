1c846d6b259104d61c31c5496a75206df7c6e87c
VaibhavVerma27 <150112360+VaibhavVerma27@users.noreply.github.com>
2025-02-16 02:37:53 +0530

    Update route.ts

--- app/api/pyqs/[id]/route.ts
+++ app/api/pyqs/[id]/route.ts
@@ -5,6 +5,60 @@ import {NextResponse} from "next/server";
 import {PyqModel} from "@/model/User";
 import mongoose from "mongoose";
 
+
+export async function GET(req: Request, { params }: { params: Promise<{ id: string }>}) {
+  try {
+    await dbConnect();
+
+    const session = await getServerSession(authOptions);
+    const user: User = session?.user as User;
+
+    if (!session || !user) {
+      return NextResponse.json({ error: 'Unauthorized. User must be logged in.' }, { status: 401 });
+    }
+
+    const userId = new mongoose.Types.ObjectId(user._id);
+
+    const { id } = await params;
+
+    if (!id) {
+      return NextResponse.json(
+        { error: "id is required" },
+        { status: 403 }
+      )
+    }
+
+    const ObjectId = new mongoose.Types.ObjectId(id);
+
+    if (!mongoose.Types.ObjectId.isValid(ObjectId)) {
+      return NextResponse.json(
+        { error: "id is not valid" },
+        { status: 403 }
+      )
+    }
+
+    const pyq = await PyqModel.findOne({
+      where: {
+        id: ObjectId,
+        author: userId
+      }
+    })
+
+    if (!pyq) {
+      return NextResponse.json(
+        { error: "failed to fetch pyq"},
+        { status: 500 }
+      )
+    }
+
+    return NextResponse.json(pyq, { status: 200 })
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: 'An error occurred while fetching pyqs.' }, { status: 500 });
+  }
+}
+
+
 export async function PATCH(req: Request, { params }: { params: Promise<{ id: string }>}) {
   try {
     await dbConnect();
